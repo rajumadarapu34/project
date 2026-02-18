65d101368ff9dca7ac1cf89fc780b7d48dcf818d
rajumadarapu34 <rajumadarapu09@gmail.com>
2026-02-18 16:31:45 +0530

    Refactor admin registration for models with custom admin

--- students/admin.py
+++ students/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from .models import Student, Subject, Marks, Attendance
 
-admin.site.register(Student)
-admin.site.register(Subject)
-admin.site.register(Marks)
-admin.site.register(Attendance)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'course', 'phone', 'admission_date')
+    search_fields = ('user__username', 'course')
+    list_filter = ('course', 'admission_date')
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Marks)
+class MarksAdmin(admin.ModelAdmin):
+    list_display = ('student', 'subject', 'score')
+    list_filter = ('subject',)
+
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('student', 'date', 'status')
+    list_filter = ('status', 'date')
